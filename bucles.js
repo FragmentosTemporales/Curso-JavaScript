6cc71130b6beb7cd790628b5f5eda9596ca8d69b
Cristian Rivera <cristian.rivera3284@gmail.com>
2022-12-23 21:47:10 -0300

    Bucles con for - while - do while

---
+++ bucles.js
@@ -0,0 +1,14 @@
+//for
+//for(let i = 0; i <= 5; i = i++){console.log(i)}
+
+//while
+//let i = 0;
+//while (i < 8) {console.log(i) i++;}
+
+//do while
+
+let i = 0;
+do {
+    i = i + 2;
+    console.log(i)
+} while (i < 20);
